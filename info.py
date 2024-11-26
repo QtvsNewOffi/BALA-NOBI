f3da6d4347c77c100dcc188bff60791c01af4b38
MessiBCN <messiinter21@gmail.com>
2024-11-26 13:51:36 +0530

    Update info.py

--- info.py
+++ info.py
@@ -37,8 +37,8 @@ MELCOW_VID = environ.get("MELCOW_VID", "https://t.me/howtodownload189/4")
 SPELL_IMG = environ.get("SPELL_IMG", "https://te.legra.ph/file/15c1ad448dfe472a5cbb8.jpg")
 
 # Admins, Channels & Users
-USERNAME = environ.get("USERNAME", "https://t.me/Anand_2K")
-ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '1735392935 ').split()]
+USERNAME = environ.get("USERNAME", "https://t.me/Itsking_45")
+ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '5937730372 ').split()]
 CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '-1002021249374').split()]
 auth_users = [int(user) if id_pattern.search(user) else user for user in environ.get('AUTH_USERS', '').split()]
 AUTH_USERS = (auth_users + ADMINS) if auth_users else []
