75d905b86013024ca08f2acaa39696a299b3a84d
Samar khan <166284907+QtvsNewOffi@users.noreply.github.com>
2025-03-02 12:29:55 +0530

    sᴍᴅ ʙᴏᴛᴢ ✨🍁

--- info.py
+++ info.py
@@ -115,7 +115,7 @@ if 'DYNO' in environ:
 else:
     ON_HEROKU = False
 BIND_ADRESS = str(getenv('WEB_SERVER_BIND_ADDRESS', '0.0.0.0'))
-FQDN = str(getenv('FQDN', BIND_ADRESS)) if not ON_HEROKU or getenv('FQDN','balanobifilt-d60df07a7298') else APP_NAME+'.herokuapp.com'
+FQDN = str(getenv('FQDN', BIND_ADRESS)) if not ON_HEROKU or getenv('FQDN') else APP_NAME+'.herokuapp.com'
 URL = "https://{}/".format(FQDN) if ON_HEROKU or NO_PORT else \
     "https://{}/".format(FQDN, PORT)
 SLEEP_THRESHOLD = int(environ.get('SLEEP_THRESHOLD', '60'))
