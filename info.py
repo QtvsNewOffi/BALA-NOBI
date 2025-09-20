b47ab0d956cc96c8c55134a19102e9ce5647de90
Samar khan <166284907+QtvsNewOffi@users.noreply.github.com>
2025-09-20 08:39:11 +0530

    sᴍᴅ ʙᴏᴛᴢ ✨🍁

--- info.py
+++ info.py
@@ -63,8 +63,8 @@ COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 VERIFY = bool(environ.get('VERIFY', False))
 TUTORIAL = environ.get('TUTORIAL', 'https://t.me/howtodownloadbestie')
 IS_TUTORIAL = bool(environ.get('IS_TUTORIAL', True))
-SHORTLINK_URL = environ.get('SHORTLINK_URL', 'shortxlinks.com')
-SHORTLINK_API = environ.get('SHORTLINK_API', 'e597d5e0ec4e3034efe7d2c5daf27eb24ef05b18')
+SHORTLINK_URL = environ.get('SHORTLINK_URL', 'arolinks.com')
+SHORTLINK_API = environ.get('SHORTLINK_API', '33257e7cd3a27f2604a76d27429ce89119573119')
 IS_SHORTLINK = bool(environ.get('IS_SHORTLINK', True))
 
 #others 
